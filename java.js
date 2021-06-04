0455ab73bb981c5dc146430355f53eb77dc57871
HaroldTheCoder <80286893+HaroldTheCoder@users.noreply.github.com>
2021-06-04 16:57:40 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,39 @@
+// Fixed Navbar
+window.addEventListener("scroll", function () {
+	var navBar = document.querySelector("#navBar");
+	var linkNav = document.querySelectorAll(".nav-link");
+	var navButton = document.querySelectorAll(".bar");
+	navBar.classList.toggle("active", window.scrollY > 5);
+	linkNav.forEach((n) => n.classList.toggle("link-active", window.scrollY > 5));
+	navButton.forEach((m) => m.classList.toggle("mobile-active", window.scrollY > 5));
+});
+
+// Button function
+window.addEventListener("scroll", function () {
+	var topButton = document.querySelector(".top-button");
+	topButton.classList.toggle("button-active", window.scrollY > 3500);
+
+});
+
+// Skill-Div Parallax function
+window.addEventListener("scroll", function () {
+	var diV = document.querySelector("#skill-section");
+	diV.classList.toggle("div-active", window.scrollY > 2100);
+
+});
+// Hamburger function
+const hamburger = document.querySelector(".nav-button");
+const navMenu = document.querySelector(".mobile-navigation");
+hamburger.addEventListener("click", openNav);
+function openNav() {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+navLink.forEach((n) => n.addEventListener("click", closeNav));
+function closeNav() {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}
+
+
