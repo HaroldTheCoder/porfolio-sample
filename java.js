b4941559a329b6cfc9f1c2c52240cfb8b2781233
HaroldTheCoder <80286893+HaroldTheCoder@users.noreply.github.com>
2021-06-06 18:11:58 -0400

    Add files via upload

--- java.js
+++ java.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function () {
 	var navBar = document.querySelector("#navBar");
 	var linkNav = document.querySelectorAll(".nav-link");
 	var navButton = document.querySelectorAll(".bar");
-	navBar.classList.toggle("active", window.scrollY > 5);
+	navBar.classList.toggle("active", window.scrollY > 0);
 	linkNav.forEach((n) => n.classList.toggle("link-active", window.scrollY > 5));
 	navButton.forEach((m) => m.classList.toggle("mobile-active", window.scrollY > 5));
 });
@@ -14,15 +14,14 @@ window.addEventListener("scroll", function () {
 	topButton.classList.toggle("button-active", window.scrollY > 4200);
 
 });
-
 // Skill-Div Parallax function
 window.addEventListener("scroll", function () {
 	var diV = document.querySelector("#skill-section");
 	var range = document.querySelectorAll(".fill");
 	var full = document.querySelectorAll(".full");
 	diV.classList.toggle("div-active", window.scrollY > 2300);
-	range.forEach((m) => m.classList.toggle("skill-active", window.scrollY > 2400));
-	full.forEach((n) => n.classList.toggle("range-active", window.scrollY > 2400)
+	range.forEach((m) => m.classList.toggle("skill-active", window.scrollY > 2650));
+	full.forEach((n) => n.classList.toggle("range-active", window.scrollY > 2650)
 	);
 });
 // Skill-Div Parallax Mobile function 
@@ -30,7 +29,7 @@ window.addEventListener("scroll", function () {
 	var diV = document.querySelector("#skill-section");
 	var range = document.querySelectorAll(".fill");
 	var full = document.querySelectorAll(".full");
-	diV.classList.toggle("div-active-mobile", window.scrollY > 10000);
+	diV.classList.toggle("div-active-mobile", window.scrollY > 15000);
 	range.forEach((m) => m.classList.toggle("skill-active-mobile", window.scrollY > 10000));
 	full.forEach((n) => n.classList.toggle("range-active-mobile", window.scrollY > 10000)
 	);
